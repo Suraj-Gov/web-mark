78f0a1fd3138aafdbacd80a0031a71362e770bf4
Suraj Govind <surajgovind332000@gmail.com>
2021-02-11 03:39:02 +0530

    tags work but cannot be removed on empty backspace

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -3,11 +3,19 @@
 import { ChangeEvent, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
+// prettier-ignore
+export const RemoveTagSVG = () => <svg viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="20" cy="20" r="20" fill="#CF3B3B"/>
+<rect x="7.09216" y="12.891" width="6.875" height="28.75" rx="3.4375" transform="rotate(-45 7.09216 12.891)" fill="#D7D7D7"/>
+<rect x="27.4215" y="8.02966" width="6.875" height="28.75" rx="3.4375" transform="rotate(45 27.4215 8.02966)" fill="#D7D7D7"/>
+</svg>
+
 const ContainerSpan = styled.span`
   position: relative;
   padding: 0.5rem 1rem;
   display: inline-block;
   margin: 1rem;
+  min-width: 8rem;
 `;
 
 const WidthSpan = styled.span`
@@ -38,6 +46,8 @@ const Tag = ({ focused, text, handleChange }: Props) => {
   const tagRef = useRef();
 
   useEffect(() => {
+    // this is showing that tagRef could be null
+    // @ts-ignore
     focused && tagRef.current.focus();
   }, []);
 
@@ -58,16 +68,32 @@ const Tag = ({ focused, text, handleChange }: Props) => {
 };
 
 const Tags = () => {
-  const [tagString, setTagString] = useState("");
+  const MAX_TAGS = 5;
   const [tags, setTags] = useState([""]);
 
   const handleTagChange = (e: ChangeEvent<HTMLInputElement>, idx: number) => {
     if (e.target.value.slice(-1) === " ") {
-      setTags((prev) => (prev.length < 5 ? prev.concat([""]) : prev));
+      if (
+        (tags.length === idx + 1 && e.target.value.length === 1) ||
+        tags.length === MAX_TAGS
+      ) {
+        return;
+      }
+      setTags((prev) => (prev.length < MAX_TAGS ? prev.concat([""]) : prev));
     }
     setTags((prev) => prev.map((p, ip) => (ip === idx ? e.target.value : p)));
-    // setTags((prev) => prev.filter((i) => i.length !== 0));
   };
+
+  const handleBackspace = (e: KeyboardEvent) => {
+    if (e.code === "Backspace") {
+      console.log(tags);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => handleBackspace(e));
+    return () => window.removeEventListener("keydown", handleBackspace);
+  }, []);
 
   return (
     <>
